86a2219486bdf7d81b3b2c6abe2bad261af6b461
Anish-Shobith <anish.shobith19@gmail.com>
2022-12-12 00:37:36 +0530

    me: tired

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
+import image from "@astrojs/image";
 
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(),
+    image({
+    serviceEntryPoint: "@astrojs/image/sharp",
+  })],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"],
