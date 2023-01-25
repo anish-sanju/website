10bf0c896fa829832fdc1ccd403b9ae917d60b68
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2023-01-25 12:12:19 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   }), react()],
